33a518e8134b518a92913a6be87d467a6dcba956
Steve Phillips <steve@tryingtobeawesome.com>
2016-07-08 16:39:34 -0700

    Tag utils: tagByPrefix{,Stripped} now accept '...prefixes' rather than 'prefix'

--- app/utils/tags.js
+++ app/utils/tags.js
@@ -1,17 +1,29 @@
 const utf8 = require('utf8');
 const atob = require('atob');
 
-export function tagByPrefix(plaintags, prefix) {
-  for (let i = 0; i < plaintags.length; i++) {
-    if (plaintags[i].startsWith(prefix)) {
-      return plaintags[i];
+export function tagByPrefix(plaintags, ...prefixes) {
+  let prefix = '';
+  for (let i = 0; i < prefixes.length; i++) {
+    prefix = prefixes[i];
+    for (let j = 0; j < plaintags.length; j++) {
+      if (plaintags[j].startsWith(prefix)) {
+        return plaintags[j];
+      }
     }
   }
   return '';
 }
 
-export function tagByPrefixStripped(plaintags, prefix) {
-  return tagByPrefix(plaintags, prefix).slice(prefix.length);
+export function tagByPrefixStripped(plaintags, ...prefixes) {
+  let tag = '';
+  for (let i = 0; i < prefixes.length; i++) {
+    tag = tagByPrefix(plaintags, prefixes[i]);
+    if (tag !== '') {
+      return tag.slice(prefixes[i].length);
+    }
+  }
+
+  return '';
 }
 
 export function tagsByPrefixStripped(plaintags, prefix) {
